bcf5598636a6f8c8fd4b9b9d261d75ccf23f9fed
Priya Sharma <77352100+DevPriSha@users.noreply.github.com>
2024-01-30 23:29:04 +0530

    Initial commit

---
+++ src/main/java/com/sample/simplydo/SimplyDoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.simplydo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/simplydo/SimplyDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.simplydo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
